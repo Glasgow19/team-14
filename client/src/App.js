ad02d9e61617815463659bb826057c6689a508b5
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-10-26 18:07:10 +0100

    [created] directory structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import STYLES from './App.css';
+// import { RouteConfig } from './routes';
+import styled from 'styled-components';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Main = styled.section`
+	display: block;
+	padding-top: 61px;
+	width: 100%;
+`;
+
+const App = () => {
+	return (
+    <div>
+
+
+      
+      {/* <Router> '/}
+			<div className={'App'}>
+				<Main>
+					<RouteConfig />
+				</Main>
+			</div>
+		{/* </Router> */}
     </div>
-  );
-}
+		
+	);
+};
 
 export default App;
