1f78c73ecb80bea0f8e08de83c12a043a0654233
Mohammad Majid <2324127m@student.gla.ac.uk>
2019-10-27 05:03:16 +0000

    Page title

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react';
-import STYLES from './App.css';
+import React, { useEffect } from "react";
+import STYLES from "./App.css";
 // import { RouteConfig } from './routes';
 
-import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { firestore } from './config/firebase.init';
-import Home from './components/Home/index';
-import NavBar from './components/Shared/NavBar';
-import ScrollContainer from './components/Shared/ScrollContainer';
-import Event from './components/Event';
-import Article from './components/Article'
-import {RouteConfig} from './routes/'
-
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { firestore } from "./config/firebase.init";
+import Home from "./components/Home/index";
+import NavBar from "./components/Shared/NavBar";
+import ScrollContainer from "./components/Shared/ScrollContainer";
+import Event from "./components/Event";
+import Article from "./components/Article";
+import { RouteConfig } from "./routes/";
+import PageTitle from "./components/PageTitle";
 
 const Main = styled.section`
-	display: block;
-	padding-top: 61px;
-	width: 100%;
+  display: block;
+  padding-top: 61px;
+  width: 100%;
 `;
 
 const App = () => {
-	return (
-		<Router>
-			<NavBar />
-      <RouteConfig/>
-		</Router>
-	);
-
+  return (
+    <Router>
+      <NavBar />
+      <PageTitle title="Home" />
+      <RouteConfig />
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/components/PageTitle/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+const PageTitle = props => {
+  return <div id="page-title">{props.title}</div>;
+};
+
+export default PageTitle;
