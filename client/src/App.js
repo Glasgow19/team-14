fe87bd52d50b8c716f151e9e6087c3f9005657dc
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-10-27 02:43:35 +0000

    [created] added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,29 @@
-import React, { useEffect } from "react";
-import STYLES from "./App.css";
+import React, { useEffect } from 'react';
+import STYLES from './App.css';
 // import { RouteConfig } from './routes';
 import styled from 'styled-components';
-import { BrowserRouter as Router } from 'react-router-dom'
-import {firestore} from './config/firebase.init'
-import Home from './components/Home/index'
-import NavBar from './components/Shared/NavBar' 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { firestore } from './config/firebase.init';
+import Home from './components/Home/index';
+import NavBar from './components/Shared/NavBar';
 import ScrollContainer from './components/Shared/ScrollContainer';
-import Event from './components/Event'
-
+import Event from './components/Event';
+import Article from './components/Article'
+import {RouteConfig} from './routes/'
 
 const Main = styled.section`
-  display: block;
-  padding-top: 61px;
-  width: 100%;
+	display: block;
+	padding-top: 61px;
+	width: 100%;
 `;
 
 const App = () => {
-  const articles = [];
-
-  useEffect(() => {
-    firestore
-      .collection("articles")
-      .get()
-      .then(data =>
-        data.forEach(doc => {
-          console.log(doc.data());
-        })
-      );
-    return () => {};
-  }, []);
-
-  return (
-    <div>
-      <NavBar />
-      <Home></Home>
-
-      <Event
-        title="Event 1"
-        location="Glasgow"
-        date="27 Oct"
-        imgSrc="http://via.placeholder.com/150"
-        backgroundColour="white"
-      />
-      <br />
-      <Event
-        title="Event 2"
-        location="Glasgow"
-        date="28 Oct"
-        imgSrc="http://via.placeholder.com/150"
-        backgroundColour="purple"
-      />
-      <br />
-      <Event
-        title="Event 3"
-        location="Edinburgh"
-        date="29 Oct"
-        imgSrc="http://via.placeholder.com/150"
-        backgroundColour="white"
-      />
-
-      {articles.forEach(a => console.log(a))}
-
-      {/* <Router> '/}
-			<div className={'App'}>
-				<Main>
-					<RouteConfig />
-				</Main>
-			</div>
-		{/* </Router> */}
-    </div>
-  );
+	return (
+		<Router>
+			<NavBar />
+      <RouteConfig/>
+		</Router>
+	);
 };
 
 export default App;
